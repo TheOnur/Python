1801c1040f6a9f2ea2ade2d6017a07bacaa178c5
Onur Bülbül <onur.bulbul@ankaref.com>
2017-06-09 17:40:39 +0300

    heap_sort added.

---
+++ sort/heap_sort.py
@@ -0,0 +1,31 @@
+def heap_sort(arr):
+    n = len(arr)
+    for i in range(n, -1 , -1):
+        def heapify(arr, i, n):
+            index = i
+            left = 2 * i + 1
+            right = 2 * i + 2
+
+            if left < n and arr[i] < arr[left]:
+                index = left
+            
+            if right < n and arr[index] < arr[right]:
+                index = right
+
+            if index != i:
+                temp = arr[index]
+                arr[index] = arr[i]
+                arr[i] = temp
+                heapify(arr, index, n)
+                
+    for i in range(n-1, 0, -1):
+        def swap(arr, i):
+            temp = arr[i]
+            arr[i] = arr[0]
+            arr[0] = temp
+        swap(arr, i)
+        heapify(arr, 0, i )
+    print(arr)
+    
+
+heap_sort([3, 12, 44, 32, 1, -1])
